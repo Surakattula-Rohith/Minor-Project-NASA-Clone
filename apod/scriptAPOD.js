dd3def283d674b26935e71516f73a7fe74cd15c3
Surakattula Rohith <srohith.charan@gmail.com>
2022-04-02 12:22:01 +0530

    02_04 first_half

--- apod/scriptAPOD.js
+++ apod/scriptAPOD.js
@@ -20,6 +20,23 @@ async function sendApiRequest() {
 
 function useApiData(data) {
 
-    document.querySelector("#pipi").innerHTML += data.explanation;
-    document.querySelector("#pipi").innerHTML += '<img src ="' + data.url + '">';
+    var x =document.querySelector("#search") ;
+    x.style.display = "none";
+    x.style.margin = "0%";
+    x.style.padding = "0%";
+
+    var y =document.querySelector("#apodimage") ;
+    y.style.display = "none";
+    y.style.margin = "0%";
+    y.style.padding = "0%";
+
+
+
+    document.querySelector("#pipi").innerHTML = '<p id="imagetitle"><span class="intro_name">Title: </span>' + data.title + '</p>' ;
+    document.querySelector("#pipi").innerHTML += '<p id="imagedate"><span class="intro_name">Image taken on: </span>' + data.date + '</p>' ;
+    document.querySelector("#pipi").innerHTML += '<p id="imageexplanation"><span class="intro_name">Context </span>:' + data.explanation + '</p>' ;
+    document.querySelector("#pipi").innerHTML += '<img id="imageurl" src ="' + data.url + '">';
+    document.querySelector("#pipi").innerHTML += '<p id="imagecopyright"><span class="intro_name">Taken By: </span>' + data.copyright + '</p>' ;
+    
+
 }
