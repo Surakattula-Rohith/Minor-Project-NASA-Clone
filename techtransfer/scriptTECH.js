8e65f02babc5ead7aed2c718384ec5063a84f540
Surakattula Rohith <srohith.charan@gmail.com>
2022-04-05 11:20:44 +0530

    05_04 10_30

--- techtransfer/scriptTECH.js
+++ techtransfer/scriptTECH.js
@@ -20,16 +20,29 @@ async function sendApiRequest() {
 
 function useApiData(data) {
 
+    var x = document.querySelector("#search");
+    x.style.display = "none";
+    x.style.margin = "0%";
+    x.style.padding = "0%";
+
+    var y = document.querySelector("#apodimage");
+    y.style.display = "none";
+    y.style.margin = "0%";
+    y.style.padding = "0%";
+
+
     // document.querySelector("#pipi").innerHTML += data.explanation;
     // document.querySelector("#pipi").innerHTML += '<img src ="' + data.url + '">';
     let text = "" ;
     let n = data.count ;
     for(let i = 0 ; i < n ; i++){
-        text += "<p>" + data.results[i][0] + "</p>" ;
-        text += "<p>" + data.results[i][1] + "</p><br>" ;
-        text += "<p>Description " + data.results[i][3] + "</p><br>" ;
-        text += "<p>Area " + data.results[i][5] + "</p><br>" ;
-        text += '<img src ="' + data.results[i][10] + '">' ;
+        text += '<br><p class="tech"><span class="intro_name"> Innovative ID Number: </span>' + data.results[i][0] + '</p>' ;
+        text += '<p class="tech"><span class="intro_name"> Tech ID Number: </span>' + data.results[i][1] + '</p>' ;
+        text += '<p class="tech"><span class="intro_name">Description: </span><br></p>' ;
+        text += '<p id="ho">' + data.results[i][3] + '</p>'  ;
+        text += '<p class="tech"><span class="intro_name">Area of Expertise: </span>' + data.results[i][5] + '</p>' ;     
+        text += '<p id="imageurl" class="tech"><img src ="' + data.results[i][10] + '"></p><br><hr>' ;
+        // data.results[i][10]
 
     }
     document.querySelector("#pipi").innerHTML += text;
