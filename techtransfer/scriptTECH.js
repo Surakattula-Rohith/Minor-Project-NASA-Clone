1f85e9c20de5cc37b61686942408aa12d8f71493
Surakattula-Rohith <srohith.charan@gmail.com>
2024-12-24 15:19:32 +0530

    Completed Tech Patents  module.

--- techtransfer/scriptTECH.js
+++ techtransfer/scriptTECH.js
@@ -1,51 +1,47 @@
 
 function buttonPress(){
-    console.log("Button Pressed");
     sendApiRequest();
 }
 
 async function sendApiRequest() {
 
     let API_KEY = "GAa8k8q7BiG5xUuGBKxrpuXCv1XjRWbHi4ImRd3J";
-
     let response = await fetch('https://api.nasa.gov/techtransfer/patent/?engine&api_key=' + API_KEY);
-    console.log(response);
-
+    
     let data = await response.json();
-    console.log(data);
-
     useApiData(data);
 
 }
 
 function useApiData(data) {
 
-    var x = document.querySelector("#search");
-    x.style.display = "none";
-    x.style.margin = "0%";
-    x.style.padding = "0%";
+    var x = document.querySelector("#loadContent");
+  x.style.display = "none";
+  x.style.margin = "0%";
+  x.style.padding = "0%";
 
-    var y = document.querySelector("#apodimage");
-    y.style.display = "none";
-    y.style.margin = "0%";
-    y.style.padding = "0%";
+  var y = document.querySelector("#bufferimage");
+  y.style.display = "none";
+  y.style.margin = "0%";
+  y.style.padding = "0%";
 
-
-    // document.querySelector("#pipi").innerHTML += data.explanation;
-    // document.querySelector("#pipi").innerHTML += '<img src ="' + data.url + '">';
-    let text = "" ;
-    let n = data.count ;
-    for(let i = 0 ; i < n ; i++){
-        text += '<br><p class="tech"><span class="intro_name"> Innovative ID Number: </span>' + data.results[i][0] + '</p>' ;
-        text += '<p class="tech"><span class="intro_name"> Tech ID Number: </span>' + data.results[i][1] + '</p>' ;
-        text += '<p class="tech"><span class="intro_name">Description: </span><br></p>' ;
-        text += '<p id="ho">' + data.results[i][3] + '</p>'  ;
-        text += '<p class="tech"><span class="intro_name">Area of Expertise: </span>' + data.results[i][5] + '</p>' ;     
-        text += '<p id="imageurl" class="tech"><img src ="' + data.results[i][10] + '"></p><br><hr>' ;
-        // data.results[i][10]
-
-    }
-    document.querySelector("#pipi").innerHTML += text;
-
-    text = "";
+  let text = "" ;
+  let n = data.count ;
+  for(let i = 0; i < n; i++){
+      text += `
+      <div class="tech-card">
+          <p class="tech-info"><span class="label">Innovative ID Number: </span><span class="value">${data.results[i][0]}</span></p>
+          <p class="tech-info"><span class="label">Tech ID Number: </span><span class="value">${data.results[i][1]}</span></p>
+          <p class="tech-info"><span class="label">Description: </span></p>
+          <p class="tech-description">${data.results[i][3]}</p>
+          <p class="tech-info"><span class="label">Area of Expertise: </span><span class="value">${data.results[i][5]}</span></p>
+          <div class="tech-image-container">
+              <img class="tech-image" src="${data.results[i][10]}" alt="Tech Image">
+          </div>
+          <hr class="divider">
+      </div>
+      `;
+  }
+  document.querySelector("#content").innerHTML += text;
+  
 }
