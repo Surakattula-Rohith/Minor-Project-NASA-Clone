37ca6254ef7f39520161c140a1518316d42a3075
Surakattula-Rohith <srohith.charan@gmail.com>
2024-12-24 15:40:41 +0530

    Completed Near Earth Objects module.

--- neows/scriptNEOWS.js
+++ neows/scriptNEOWS.js
@@ -1,79 +1,68 @@
-var present_date ;
+var present_date;
 function buttonPress() {
-    console.log("Button Pressed");
-    sendApiRequest();
+  sendApiRequest();
 }
 
 async function sendApiRequest() {
+  let API_KEY = "GAa8k8q7BiG5xUuGBKxrpuXCv1XjRWbHi4ImRd3J";
+  present_date = "2024-12-23";
+  let response = await fetch(
+    "https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-12-16&end_date=2024-12-23&api_key=" +
+      API_KEY
+  );
 
-    let API_KEY = "GAa8k8q7BiG5xUuGBKxrpuXCv1XjRWbHi4ImRd3J";
-
-    // const d = new Date();
-    // let day = d.getUTCDate();
-    // let month = d.getUTCMonth();
-    // let year = d.getUTCFullYear();
-    // let month_val = "";
-    // if (month < 10) {
-    //     month_val += "0";
-    // }
-    // month_val += month;
-
-    // let day_val = "";
-    // if (day < 10) {
-    //     day_val += "0";
-    // }
-    // day_val += day;
-
-    // let year_val = "";
-    // year_val += year;
-
-    present_date = "2022-04-20";
-
-    let response = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-04-15&end_date=2022-04-20&api_key=' + API_KEY);
-    console.log(response);
-
-    let data = await response.json();
-    console.log(data);
-
-    useApiData(data);
-
+  let data = await response.json();
+  useApiData(data);
 }
 
 function useApiData(data) {
-    // document.querySelector("#pipi").innerHTML += date;
+  var x = document.querySelector("#loadContent");
+  x.style.display = "none";
+  x.style.margin = "0%";
+  x.style.padding = "0%";
 
-    var x = document.querySelector("#search");
-    x.style.display = "none";
-    x.style.margin = "0%";
-    x.style.padding = "0%";
+  var y = document.querySelector("#bufferimage");
+  y.style.display = "none";
+  y.style.margin = "0%";
+  y.style.padding = "0%";
 
-    var y = document.querySelector("#apodimage");
-    y.style.display = "none";
-    y.style.margin = "0%";
-    y.style.padding = "0%";
+  let text = "<table class='styled-table'>";
 
-    let text = "<table>";
+  text +=
+    "<thead><tr><th>ID</th><th>NAME</th><th>Diameter (ft)</th><th>Relative Velocity (km/h)</th><th>Closest Approach</th></tr></thead>";
+  text += "<tbody>";
+  let n = data.near_earth_objects[present_date].length;
+  for (let i = 0; i < n; i++) {
+    text += "<tr class='data-row'>";
 
-    //near_earth_objects["2022-03-29"][0].id
-    text += "<tr><th>ID</th><th>NAME</th><th>Diameter</th><th>Relative Velocity</th><th>Closest approach</th></tr>";
-    let n = data.near_earth_objects[present_date].length;
-    for (let i = 0; i < n; i++){
-        text += "<tr>" ;
-        
-            text += "<td>" + data.near_earth_objects[present_date][0].id + "</td>";
-            text += "<td>" + data.near_earth_objects[present_date][i].name + "</td>";
-            text += "<td>" + data.near_earth_objects[present_date][i].estimated_diameter.feet.estimated_diameter_min + "</td>";
-            text += "<td>" + data.near_earth_objects[present_date][i].close_approach_data[0].relative_velocity.kilometers_per_hour + "</td>";
-            text += "<td>" + data.near_earth_objects[present_date][i].close_approach_data[0].close_approach_date_full + "</td>";
+    text +=
+      "<td class='data-id'>" +
+      data.near_earth_objects[present_date][i].id +
+      "</td>";
+    text +=
+      "<td class='data-name'>" +
+      data.near_earth_objects[present_date][i].name +
+      "</td>";
+    text +=
+      "<td class='data-diameter'>" +
+      data.near_earth_objects[present_date][i].estimated_diameter.feet
+        .estimated_diameter_min +
+      "</td>";
+    text +=
+      "<td class='data-velocity'>" +
+      data.near_earth_objects[present_date][i].close_approach_data[0]
+        .relative_velocity.kilometers_per_hour +
+      "</td>";
+    text +=
+      "<td class='data-approach'>" +
+      data.near_earth_objects[present_date][i].close_approach_data[0]
+        .close_approach_date_full +
+      "</td>";
 
-        text += "</tr>" ;
-    }
+    text += "</tr>";
+  }
 
-    text += "</table>";
-    document.querySelector("#pipi").innerHTML += text;
-
-    text = "";
-
-
-
+  text += "</tbody>";
+  text += "</table>";
+  document.querySelector("#content").innerHTML += text;
 }
